1f5e91aebbf850e47d39257593491ee92ee9f35b
hongchengkuan <hongckuan@gmail.com>
2016-11-18 23:40:18 -0800

    Create precedence_test.sh

---
+++ tests/precedence_test.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+(echo A)
+(echo A || echo B)
+(echo A && echo B)
+(echo A || echo B && echo C)
+(echo A && echo B && echo C)
+echo A || (echo B || echo C)
+echo A || (echo B && echo C)
+echo A && (echo B || echo C)
+echo A && (echo B && echo C)
+
+echo A || (echo B || echo C); echo D
+echo A || (echo B && echo C) || echo D
+echo A && (echo B || echo C) && echo D
+echo A && (echo B && echo C)  && echo D; echo P
+
+(echo A || (echo B) || echo C)&& echo D
+(echo A || (echo B && echo C) || echo D)
+(echo A && (echo B || echo C)) && echo D
+echo A && (echo B && echo C)  && (echo D || echo P)
+
+echo A && echo B || echo C && echo D
+(echo A && echo B) || (echo C && echo D)
+
+((echo A && echo B) || echo C && echo D)
+
+((echo A && echo B) || (echo C && echo D))
+
+((echo A && echo B) || echo C && echo D)))
+((echo A && echo B) || echo C && echo D))
+(echo A && echo B) || echo C && echo D)
+echo A && echo B) || echo C && echo D)
+
+
+
+echo Generating bug
